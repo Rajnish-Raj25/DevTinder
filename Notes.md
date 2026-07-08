f88a1c4a1d123ad48fc7f76bc68b01abe4584a64
Rajnish-Raj25 <rajnishraj268@gmail.com>
2026-07-08 22:36:01 +0530

    added route handler and tested dummy api in postman

---
+++ Notes.md
@@ -0,0 +1,1 @@
+this is the Note.md document
--- src/app.js
+++ src/app.js
@@ -1,8 +1,37 @@
  const  express = require('express');
  const app =express();
+
+
+
+ /**
+ * @description Handle GET request to /user
+ */
+ // handle GET request to /user
+ app.get(("/user"),(req,res)=>{
+   res.send({statusCode: 200, name: "John", age: 30})
+ })
+
+ /**
+ * @description Handle POST request to /user
+ */
+ //handle post request to /user
+ app.post(("/user"),(req,res)=>{
+   res.send({statusCode: 201, message: "User created successfully", data: req.body})
+   console.log(req);
+ })
+
+ /**
+ * @description Handle DELETE request to /user
+ */
+ //handle delete request to /user
+ app.delete(("/user"),(req,res)=>{
+   res.send({statusCode: 200, message: "User deleted successfully"})
+ })
+
+ //handle all requests to /abc
  app.use(("/abc"),(req,res)=>{
     console.log('Hello from middleware',req.url);
-    
+
     res.send('Hello World')
 
  })
