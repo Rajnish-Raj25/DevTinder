2b1c9f351f1a6ba68785aff274a46d7c5ab44bb3
Rajnish-Raj25 <rajnishraj268@gmail.com>
2026-07-09 09:08:12 +0530

    playing with routing

--- src/app.js
+++ src/app.js
@@ -7,34 +7,12 @@
  * @description Handle GET request to /user
  */
  // handle GET request to /user
- app.get(("/user"),(req,res)=>{
-   res.send({statusCode: 200, name: "John", age: 30})
+ app.get(("/user/:name/:id/:age"),(req,res)=>{
+   res.send({statusCode: 200, name: req.params.name, age: req.params.age, id: req.params.id})
+   console.log(req.params)
  })
 
- /**
- * @description Handle POST request to /user
- */
- //handle post request to /user
- app.post(("/user"),(req,res)=>{
-   res.send({statusCode: 201, message: "User created successfully", data: req.body})
-   console.log(req);
- })
 
- /**
- * @description Handle DELETE request to /user
- */
- //handle delete request to /user
- app.delete(("/user"),(req,res)=>{
-   res.send({statusCode: 200, message: "User deleted successfully"})
- })
-
- //handle all requests to /abc
- app.use(("/abc"),(req,res)=>{
-    console.log('Hello from middleware',req.url);
-
-    res.send('Hello World')
-
- })
 
  app.listen(3000,()=>{
     console.log('Server is running on port 3000');
