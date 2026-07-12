9caa04f8742a9ac8976d42c4d2a804f08ebfc72f
Rajnish-Raj25 <rajnishraj268@gmail.com>
2026-07-12 12:04:05 +0530

    added signUp api  and with middleware

--- src/app.js
+++ src/app.js
@@ -3,23 +3,18 @@ const connectDB = require('./config/database');
   const User = require("./models/user");
  const app =express();
  
+ app.use(express.json());
 
 
 app.post("/signUp", async (req, res) => {
-    const {firstName,lastName,email,password,age,gender} = req.query;
-    const newUser = new User({
-        firstName,
-        lastName,
-        email,
-        password,
-        age,
-        gender
-    });
+
+
+    const newUser = new User(req.body);
 
    
 
 try{
-     await newUser.save()
+     await newUser.save();
      res.status(201).json({ message: "User created successfully" });
 }catch(err){
      res.status(500).json({ "error on save": err.message });
@@ -27,15 +22,6 @@ try{
 });
 
 
-app.get("/getUser",(req,res)=>{
-    const alluser = User.find()
-    .then((users) => {
-        res.status(200).json(users);
-    })
-    .catch((err) => {
-        res.status(500).json({ error: err.message });
-    });
-})
 
 connectDB().then(()=>{
     console.log("Database connected successfully");
